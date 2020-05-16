9a1d3cd1068d30b4f069c70e96950ce0cff83891
TamiKabiawu <tamihawk12@gmail.com>
2020-05-16 16:47:46 -0400

    This is the second commit. I finshed the menu by itself

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hthsHacks
 hthsHacks Repository
+
+Must install python imaging libary: pip install Pillow
+For the imaging that is on the Menu
--- menu.py
+++ menu.py
@@ -1,16 +1,26 @@
 from tkinter import *
 from tkinter import messagebox
+from PIL import ImageTk, Image
 
-
-#this is just laying out the basics of the GUI
+#this is just laying out the basics window of the GUI
 top = Tk()
 top.geometry("500x570")
 top.title("Animal Watcher")
+
+#This is used to Style the top of the Window
 frame = Frame(top, relief=RIDGE, borderwidth=2)
 frame.pack(fill=BOTH,expand=1)
 frame.config(background='light blue')
 label = Label(frame,text="Animal Watcher", bg='light blue', font=('Times 35 bold'))
 label.pack(side=TOP)
+
+#creates a Tkinter iamge that can be used everywhere Tkinter expects an image object
+path = "Fall-beautiful-nature.jpg"
+img = ImageTk.PhotoImage(Image.open(path))
+
+#This is used to insert the Image
+background_label = Label(top, image=img)
+background_label.pack(side=TOP)
 
 #this is a sample function called hello
 def hello():
