fe0c375bb3f679fdc9d87e3d54ee6c01249b9945
TamiKabiawu <tamihawk12@gmail.com>
2020-05-16 17:06:59 -0400

    This is a commit before I try to fix something

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ hthsHacks Repository
 
 Must install python imaging libary: pip install Pillow
 For the imaging that is on the Menu
+
+Must install cv2: pip install opencv-python
+For the Detection algorithm
+
+Must install Numpy: pip install numpy
+For the detection algorithm 
+(Make sure numpy is up to date by pip install -U numpy)
--- menu.py
+++ menu.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from tkinter import messagebox
 from PIL import ImageTk, Image
+import os 
+import detection
 
 #this is just laying out the basics window of the GUI
-top = Tk()
-top.geometry("500x570")
-top.title("Animal Watcher")
+root = Tk()
+root.geometry("500x570")
+root.title("Animal Watcher")
 
 #This is used to Style the top of the Window
-frame = Frame(top, relief=RIDGE, borderwidth=2)
+frame = Frame(root, relief=RIDGE, borderwidth=2)
 frame.pack(fill=BOTH,expand=1)
 frame.config(background='light blue')
 label = Label(frame,text="Animal Watcher", bg='light blue', font=('Times 35 bold'))
@@ -19,17 +21,16 @@ path = "Fall-beautiful-nature.jpg"
 img = ImageTk.PhotoImage(Image.open(path))
 
 #This is used to insert the Image
-background_label = Label(top, image=img)
+background_label = Label(root, image=img)
 background_label.pack(side=TOP)
 
-#this is a sample function called hello
-def hello():
-    messagebox.showinfo("Say Hello", "Hello World")
-
-#command is the function or method to be called when the button is pressed
-B1 = Button(padx=5,pady=5, text = "START WATCHING", font=('helvetica 15 bold'), bg= 'white', fg='black', relief=GROOVE, command=hello)
+#This is what lays out the button
+B1 = Button(padx=5,pady=5, text = "START WATCHING", font=('helvetica 15 bold'), bg= 'white', fg='black', relief=GROOVE, command=detectionAlg)
 B1.place(x=165, y=104)
 
+#this is a function that calls the detection algorithm in the detection.py file
+def detectionAlg():
+    os.system('python detection.py')
 
-top.mainloop()
+root.mainloop()
 
